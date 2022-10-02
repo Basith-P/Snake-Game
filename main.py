fe35e8ac77af2f8593531d67aa5670211db6af02
Basith P <basithpofficial@gmail.com>
2022-10-02 07:57:17 +0530

    :sparkles: Change direction on keypress

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import time
 
 from snake import Snake
 
-s = Screen()
-s.setup(width=600, height=600)
-s.bgcolor("black")
-s.title("Snake Game")
-s.tracer(0)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_over = False
 
 while not game_over:
-    s.update()
+    screen.update()
     time.sleep(.1)
 
     snake.move()
 
-s.exitonclick()
+screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 INITIAL_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -9,6 +13,7 @@ class Snake:
     def __init__(self):
         self.blocks = []
         self.create_snake()
+        self.head = self.blocks[0]
 
     def create_snake(self):
         for i in INITIAL_POS:
@@ -23,4 +28,20 @@ class Snake:
             new_x = self.blocks[block_index - 1].xcor()
             new_y = self.blocks[block_index - 1].ycor()
             self.blocks[block_index].goto(new_x, new_y)
-        self.blocks[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
